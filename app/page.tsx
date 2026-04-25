63fabd0554601a7acfd6b2e1508ebb192526bc7e
inhwoi7 <inhwoi7@gmail.com>
2026-04-25 14:01:35 +0900

    MBTI 매칭

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ const SERVICES = [
   },
   {
     key: "mbti",
-    name: "MBTI",
+    name: "MBTI 매칭",
     description: "성향과 관계 패턴을 쉽고 명확하게 이해해보세요.",
     href: "https://dasangdam-mbti-sunny.vercel.app",
     iconBg: "#EAF4FF",
